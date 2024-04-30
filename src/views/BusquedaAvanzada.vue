10aec6f9a3f8a504de0f6b8adc7dca45bdfe6e6d
SrMazapan <jemmanuelma@outlook.com>
2024-04-29 22:18:55 -0600

    diseño parcial del home y edicion de formularios

--- src/views/BusquedaAvanzada.vue
+++ src/views/BusquedaAvanzada.vue
@@ -1,65 +1,63 @@
 <template>
-    <ButtonSearch/>
-    <div class="d-flex justify-content-center">
-    <div class="card m-3" style="width: 100%; max-width: 600px;">
-        <div class="card-body">
-            <form @submit.prevent="handleSubmit">
-                <fieldset>
-                    <legend class="card-title" style="text-align: center;">Busqueda Avanzada</legend>
-                    <div class="mb-3">
+  <ButtonSearch/>
+  <div class="d-flex justify-content-center">
+  <div class="card m-3" style="width: 100%; max-width: 600px;">
+      <div class="card-body">
+          <form @submit.prevent="handleSubmit">
+              <fieldset>
+                  <legend class="card-title" style="text-align: center;">Busqueda Avanzada</legend>
+                  <div class="mb-3">
                     <label for="TextInput" class="form-label" >Escriba una palabra o frase</label>
                     <input type="text" id="TextInput" class="form-control" placeholder="Escribe una palabra que se relacione con tu búsqueda" v-model="word">
-                    </div>
-                    <div class="mb-3">
-                        <label for="disabledSelect" class="form-label">Campo de búsqueda</label>
-                        <select id="disabledSelect" class="form-select" v-model="searchField">
-                            <option>Todos los campos</option>
-                            <option>Título exacto</option>
-                            <option>Título</option>
-                            <option>Autor</option>
-                            <option>Materia</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="startYear" class="form-label">Desde el año: </label>
-                        <input type="number" id="startYear" class="form-control" placeholder="(aaaa)" v-model="startYear">
-                    </div>
-                    <div class="mb-3">
-                        <label for="endYear" class="form-label">Hasta el año: </label>
-                        <input type="number" id="endYear" class="form-control" placeholder="(aaaa)" v-model="endYear">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </fieldset>
-            </form>
-        </div>
-    </div>  
-    </div>
-    <div v-if="errorMessage" class="custom-error-message">
-          {{ errorMessage }}
-    </div>
+                  </div>
+                  <div class="mb-3">
+                    <label for="TextTitle" class="form-label" >Título</label>
+                    <input type="text" id="TextTitle" class="form-control" placeholder="Título o tema" v-model="title">
+                  </div>
+                  <div class="mb-3">
+                    <label for="TextAutor" class="form-label" >Autor</label>
+                    <input type="text" id="TextAutor" class="form-control" placeholder="Autor de la tesis" v-model="autor">
+                  </div>
+                  <div class="mb-3">
+                    <label for="TextMateria" class="form-label" >Materia</label>
+                    <input type="text" id="TextMateria" class="form-control" placeholder="Materia a la que pertenece" v-model="materia">
+                  </div>
+                  <div class="mb-3">
+                    <label for="startYear" class="form-label">Desde el año: </label>
+                    <input type="number" id="startYear" class="form-control" placeholder="(aaaa)" v-model="startYear">
+                  </div>
+                  <div class="mb-3">
+                    <label for="endYear" class="form-label">Hasta el año: </label>
+                    <input type="number" id="endYear" class="form-control" placeholder="(aaaa)" v-model="endYear">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+              </fieldset>
+          </form>
+      </div>
+  </div>  
+  </div>
+  <div v-if="errorMessage" class="custom-error-message">
+        {{ errorMessage }}
+  </div>
 
 </template>
 
 <script setup>
-    import { ref } from "vue"
-    import ButtonSearch from "../components/buttonSearch.vue";
+  import { ref } from "vue"
+  import ButtonSearch from "../components/buttonSearch.vue";
 
 const word = ref("")
-const searchField = ref("")
+const title = ref("")
+const autor = ref("")
+const materia = ref("")
 const startYear = ref("")
 const endYear = ref("")
 const errorMessage = ref("")
 
 const handleSubmit = async () => {
 
-  if (!word.value) {
-    errorMessage.value = "Por favor, ingresa una palabra o frase."
-    setTimeout(() => {errorMessage.value = ''}, 2000)
-    return
-  }
-
-  if (searchField.value === "") {
-    errorMessage.value = "Por favor, selecciona un campo de búsqueda."
+  if (!word.value || !title.value || !autor.value || !materia.value) {
+    errorMessage.value = "Todos los campos son obligatorios."
     setTimeout(() => {errorMessage.value = ''}, 2000)
     return
   }
@@ -81,8 +79,6 @@ const handleSubmit = async () => {
     setTimeout(() => {errorMessage.value = ''}, 2000)
     return
   }
-  
-  // Aquí va el resto de tu código
 }
 
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-     <div>
-         <ButtonSearch/>
-     </div>
+    <div>
+        <ButtonSearch/>
+    </div>
+    <div class="container text-center py-5">
+        <h1 class="display-4 my-3">¡Bienvenido a nuestro repositorio de tesis!</h1>
+        <p class="lead text-muted">El Repositorio escolar es una plataforma digital que proporciona acceso abierto 
+            en texto completo a diversos recursos de información académica. En el Repositorio se pueden consultar tesis 
+            elaboradas en esta institucion de educación superior. Navega por nuestras secciones y descubre todo lo que 
+            tenemos para ofrecerte.</p>
+    </div>
+    <div class="d-flex flex-column text-center">
+        <hr class="my-4">
+    </div>
 </template>
 
 <script setup>
 import ButtonSearch from '../components/buttonSearch.vue';
 import { useUserStore } from '../stores/user';
-// import BusquedaBasica from './BusquedaBasica.vue';
 const userStore = useUserStore()
 
 </script>
